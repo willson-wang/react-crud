40398cb8387b2f683c004471a51a324e88b85f82
wangks <wangkangsen168@163.com>
2018-10-27 22:39:06 +0800

    通过yarn eject放出配置项，并打开css-loader的module功能

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import styles from '../assets/css/app.module.css';
+import app from '../assets/css/app.module.css';
 
 class App extends Component {
   render() {
-    console.log(styles)
     return (
-      <div className={styles.wrap}>
+      <div className={app.wrap}>
         hello world!
       </div>
     );
