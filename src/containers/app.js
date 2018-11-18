f5cc0e719dc583f99edc594a524a9543c6fd7bb1
wangks <wangkangsen168@163.com>
2018-11-19 00:04:35 +0800

    将路由方式由BrowserRouter改成HashRouter

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import app from '../assets/css/app.module.css';
 import Layout from './layout';
 import Login from './login';
